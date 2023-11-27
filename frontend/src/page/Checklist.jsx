e0321c4d9ab429977a953da60d7e0217fbab0b85
[Tomas Valdez] <Tomasvaldez02@gmail.com>
2023-11-27 15:51:41 -0300

    modificaciones de estilos a checklist, botonscket y modificaciones a formularios

--- frontend/src/page/Checklist.jsx
+++ frontend/src/page/Checklist.jsx
@@ -1,241 +1,279 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navbar } from "../components/Navbar.jsx";
 import { BotonSocket } from "../components/BotonSocket.jsx";
 import { Footer } from "../components/Footer.jsx";
-
-
+import "../public/css/checklist.css";
 
 export default function Checklist() {
   const [valorFinal, setValorFinal] = useState(0);
   const [porcentaje, setPorcentaje] = useState(0);
 
-
-  const [valorSection1, setValorSection1] =  useState(0)
-  const [valorSection2, setValorSection2] =  useState(0)
-  const [valorSection3, setValorSection3] =  useState(0)
+  const [valorSection1, setValorSection1] = useState(0);
+  const [valorSection2, setValorSection2] = useState(0);
+  const [valorSection3, setValorSection3] = useState(0);
 
   const handleCheckboxChange = (e) => {
-
     const elementoOrigen = e.target;
     if (elementoOrigen.checked) {
       setValorFinal((prevValor) => {
         const updatedValor = prevValor + 1;
-        setPorcentaje((updatedValor * 100 / 15));
-        return updatedValor
+        setPorcentaje((updatedValor * 100) / 15);
+        return updatedValor;
       });
-      switch(true){
+      switch (true) {
         case elementoOrigen.classList.contains("ig-check"):
           // código si tiene la claseIG
-          setValorSection1((prevIG) => prevIG + 1)
-        break;
+          setValorSection1((prevIG) => prevIG + 1);
+          break;
         case elementoOrigen.classList.contains("sg-check"):
           // código si tiene la claseSG
-          setValorSection2((prevSG) => prevSG + 1)
-        break;
+          setValorSection2((prevSG) => prevSG + 1);
+          break;
         case elementoOrigen.classList.contains("hg-check"):
           // código si tiene la claseHG
-          setValorSection3((prevHG) => prevHG + 1)
-        break;
+          setValorSection3((prevHG) => prevHG + 1);
+          break;
         default:
           // código por defecto si no coincide con ninguna clase
-        break;
+          break;
       }
-      
-    }else if(!elementoOrigen.checked) {
+    } else if (!elementoOrigen.checked) {
       setValorFinal((prevValor) => {
         const updatedValor = prevValor - 1;
-        setPorcentaje(updatedValor * 100 / 15);
-        return updatedValor
+        setPorcentaje((updatedValor * 100) / 15);
+        return updatedValor;
       });
-      switch(true){
+      switch (true) {
         case elementoOrigen.classList.contains("ig-check"):
           // código si tiene la clase1
-          setValorSection1((prevIG) => prevIG - 1)
-        break;
+          setValorSection1((prevIG) => prevIG - 1);
+          break;
         case elementoOrigen.classList.contains("sg-check"):
           // código si tiene la clase2
-          setValorSection2((prevSG) => prevSG - 1)
-        break;
+          setValorSection2((prevSG) => prevSG - 1);
+          break;
         case elementoOrigen.classList.contains("hg-check"):
           // código si tiene la clase3
-          setValorSection3((prevHG) => prevHG - 1)
-        break;
+          setValorSection3((prevHG) => prevHG - 1);
+          break;
         default:
           // código por defecto si no coincide con ninguna clase
-        break;
+          break;
       }
     }
+  };
 
+  useEffect(() => {
+    const imprimirBtn = document.querySelector("#imprimirBtn");
 
-  }
+    const handleImprimirClick = () => {
+      let botoncheck = document.getElementById("botoncheck");
+      botoncheck.style.display = "none";
 
+      const checks = document.querySelectorAll(".mycheck");
+      checks.forEach((check) => check.classList.remove("mycheck"));
+
+      window.print();
+
+      botoncheck.style.display = "block";
+      checks.forEach((check) => check.classList.add("mycheck"));
+    };
+
+    imprimirBtn.addEventListener("click", handleImprimirClick);
+
+    return () => {
+      imprimirBtn.removeEventListener("click", handleImprimirClick);
+    };
+  }, []);
 
   return (
     <>
       <Navbar />
-      <form>
-        {/* ... */}
-        <section>
+      <section className="body">
+        <form className="form">
           {/* ... */}
-          <div>
-            <h3>Instalaciones Generales: {valorSection1}/5</h3>
-            <label>
-              <input
-                type="checkbox"
-                className="ig-check"
-                value="1"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-            <label>
-              <input
-                type="checkbox"
-                className="ig-check"
-                value="2"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-            <label>
-              <input
-                type="checkbox"
-                className="ig-check"
-                value="3"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-            <label>
-              <input
-                type="checkbox"
-                className="ig-check"
-                value="4"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-            <label>
-              <input
-                type="checkbox"
-                className="ig-check"
-                value="5"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
+          <section className="section">
+            {/* ... */}
+            <div>
+              <h3 className="h3">Instalaciones Generales: {valorSection1}/5</h3>
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="ig-check mycheck input"
+                  value="1"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="ig-check mycheck input"
+                  value="2"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="ig-check mycheck input"
+                  value="3"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="ig-check mycheck input"
+                  value="4"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="ig-check mycheck input"
+                  value="5"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+            </div>
+            <div>
+              <h3 className="h3">Seguridad: {valorSection2}/5</h3>
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="sg-check mycheck input"
+                  value="1"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="sg-check mycheck input"
+                  value="2"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="sg-check mycheck input"
+                  value="3"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="sg-check mycheck input"
+                  value="4"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="sg-check mycheck input"
+                  value="5"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+            </div>
+            <div>
+              <h3 className="h3">Higiene: {valorSection3}/5</h3>
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="hg-check mycheck input"
+                  value="1"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="hg-check mycheck input"
+                  value="2"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="hg-check mycheck input"
+                  value="3"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="hg-check mycheck input"
+                  value="4"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+              <label className="label">
+                <input
+                  type="checkbox"
+                  className="hg-check mycheck input"
+                  value="5"
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Existen salidas de emergencia claramente señalizadas.
+              </label>
+              <br />
+            </div>
+          </section>
+          <div id="botoncheck">
+            <input type="button" value="Imprimir" id="imprimirBtn" />
           </div>
-          <div>
-            <h3>Seguridad: {valorSection2}/5</h3>
-            <label>
-              <input
-                type="checkbox"
-                className="sg-check"
-                value="1"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-            <label>
-              <input
-                type="checkbox"
-                className="sg-check"
-                value="2"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-            <label>
-              <input
-                type="checkbox"
-                className="sg-check"
-                value="3"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-            <label>
-              <input
-                type="checkbox"
-                className="sg-check"
-                value="4"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-            <label>
-              <input
-                type="checkbox"
-                className="sg-check"
-                value="5"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-          </div>
-          <div>
-            <h3>Higiene: {valorSection3}/5</h3>
-            <label>
-              <input
-                type="checkbox"
-                className="hg-check"
-                value="1"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-            <label>
-              <input
-                type="checkbox"
-                className="hg-check"
-                value="2"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-            <label>
-              <input
-                type="checkbox"
-                className="hg-check"
-                value="3"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-            <label>
-              <input
-                type="checkbox"
-                className="hg-check"
-                value="4"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-            <label>
-              <input
-                type="checkbox"
-                className="hg-check"
-                value="5"
-                onChange={handleCheckboxChange}
-              />{" "}
-              Existen salidas de emergencia claramente señalizadas.
-            </label><br />
-          </div>
-        </section>
-      </form>
-      <hr style={{ 
-        color: "black",
-        backgroundColor: "black",
-        height: 2,
-        width: 20 + "px",
-        margin: 20 + "px"
-      }}/>
-      
-      <h2>Se cumplen {valorFinal} de 15 reglas</h2>
-      <p>Cumplimiento: {(porcentaje).toFixed(2) + "%"}</p>
-      <BotonSocket porcentaje={porcentaje}>
-      </BotonSocket>
+        </form>
+        <hr
+          style={{
+            color: "black",
+            backgroundColor: "black",
+            height: 2,
+            width: 20 + "px",
+            margin: 20 + "px",
+          }}
+        />
+
+        <h2 className="h2">Se cumplen {valorFinal} de 15 reglas</h2>
+        <p>Cumplimiento: {porcentaje.toFixed(2) + "%"}</p>
+        <BotonSocket porcentaje={porcentaje}></BotonSocket>
+      </section>
 
       <Footer />
     </>
-  );}
+  );
+}
--- frontend/src/page/Formularios.jsx
+++ frontend/src/page/Formularios.jsx
@@ -1,10 +1,10 @@
 import "../public/css/formulario.css";
 import { Navbar } from "../components/Navbar.jsx";
 import { Link } from "react-router-dom";
-import safety from "../public/images/safety.png"
-import drive from "../public/images/drive.png"
-import guarantee from "../public/images/guarantee.png"
-import formulario1 from "../public/images/formulario1.png"
+import safety from "../public/images/safety.png";
+import drive from "../public/images/drive.png";
+import guarantee from "../public/images/guarantee.png";
+import formulario1 from "../public/images/formulario1.png";
 import { Footer } from "../components/Footer.jsx";
 
 export default function Formularios() {
@@ -18,28 +18,31 @@ export default function Formularios() {
           </h1>
           <div className="buttons-about">
             <Link className="button-about" to="/checklist">
-              Seguridad
+              inst. Estudiantil
             </Link>
-            <a className="button-about2" href="/higiene">
-              Higiene
-            </a>
+            <Link className="button-about2" to="/checklist">
+              Seg. en planta ind.
+            </Link>
+            <Link className="button-about2" to="/checklist">
+              Seg. obra cons.
+            </Link>
             <ul className="hero-info">
               <li>
-              <div className="container-img">
-                <img src={safety} alt="safety-img" />
-              </div>
+                <div className="container-img">
+                  <img src={safety} alt="safety-img" />
+                </div>
                 <h4>100% Seguro</h4>
               </li>
               <li>
-              <div className="container-img">
-                <img src={guarantee} alt="guarante-img" />
-              </div>
+                <div className="container-img">
+                  <img src={guarantee} alt="guarante-img" />
+                </div>
                 <h4>Confiable</h4>
               </li>
               <li>
-              <div className="container-img">
-                <img src={drive} alt="drive-img" />
-              </div>
+                <div className="container-img">
+                  <img src={drive} alt="drive-img" />
+                </div>
                 <h4>
                   Buen <br /> salvaguardado
                 </h4>
